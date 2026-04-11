46d71e3bdcc38980241009348d35ec4603321373
Stephan Kern <stephan@swmmrman.com>
2026-04-11 03:08:26 -0600

    Add relay and startup test

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import network
 import urequests
 import wifi
 
+relay_pin = 14
+
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(wifi.ssid, wifi.password)
@@ -16,6 +18,12 @@ while wlan.status() != 3:
     time_nc = time_nc + 1
 
 print(f"\r\nWifi connected: {wlan.ifconfig()}")
+
+relay = machine.Pin(relay_pin, machine.Pin.OUT)
+relay.on()
+time.sleep(5)
+relay.off()
+
 
 while True:
     resp = urequests.get('http://192.168.1.134/temp_in.txt')
