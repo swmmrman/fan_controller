fa1d5155baf1d8e0d74acc510e0080ba212e8f98
Stephan Kern <stephan@swmmrman.com>
2026-04-11 02:12:40 -0600

    Add a delay and check for wifi starting

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(wifi.ssid, wifi.password)
 
-while wlan.status() != 3:
-    print("\rNo wifi", end="")
-    time.sleep(1)
 
-print(f"\rWifi connected: {wlan.ifconfig()}")
+time_nc = 0
+while wlan.status() != 3:
+    print(f"\rNo wifi, {time_nc} seconds", end="")
+    time.sleep(1)
+    time_nc = time_nc + 1
+
+print(f"\r\nWifi connected: {wlan.ifconfig()}")
 
 while True:
     resp = urequests.get('http://192.168.1.134/temp_in.txt')
