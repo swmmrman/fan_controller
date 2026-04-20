a5e306f5719321d1c1f8e56e01cf33b551d1f016
Stephan Kern <stephan@swmmrman.com>
2026-04-20 03:27:48 -0600

    Stray header

--- diags.py
+++ diags.py
@@ -7,7 +7,6 @@ def re(s,mesg):
     gc_status = gc.isenabled()
     lt = time.localtime(time.time()-21600)
     t = "{: 2}:{:02}:{:02} {}-{:02}-{:02}".format(lt[3],lt[4],lt[5],lt[0],lt[1],lt[2])
-    sock.send(f"{header}")
     sock.send(f"{header}Your IP:{addr[0]}\r\nFree Memory: {free_mem:,}\r\nGC Enanbled: {gc_status}\r\n")
     sock.send(f"Current Time:{t}\r\n{mesg}\r\nCurrent Temps\r\nSensor 1:{temp_1: 4.2f}f\r\nSensor 2:{temp_2: 4.2f}f\r\n\r\n")
     sock.send("\r\n")
